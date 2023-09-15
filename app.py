22fee3168e25e1916dd3a77819a42e771a81c0aa
dedgar1 <jnzokwe@gmail.com>
2023-09-15 18:58:15 -0400

    feat: :sparkles: create and view posts one at a time
    able to create and view posts one at a time

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, app
 from routes.post import post_pages
+from sqlmodel import SQLModel, create_engine
+from models.post import Post
 
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(post_pages)
-    return app
+def init_app():
+    """Initialize the core application."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.engine = create_engine("sqlite:///database.db")
+
+    with app.app_context():
+        # before_first_request equivalent here
+        SQLModel.metadata.create_all(app.engine)
+
+        # Register Blueprints
+        app.register_blueprint(post_pages)
+        return app
+
+
+
+if __name__ == "__main__":
+    app = init_app()
+    app.run(host='0.0.0.0', port=5001)
--- routes/post.py
+++ routes/post.py
@@ -1,13 +1,24 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, render_template, redirect,url_for, current_app
+from sqlmodel import Session, select
+from models.post import Post
 
 post_pages = Blueprint("post", __name__)
 
 @post_pages.get("/post/<string:title>")
 def display_post(title: str):
-    return "Display post pages"
+    with Session(current_app.engine) as session:
+        statement = select(Post).where(Post.title == title)
+        post = session.exec(statement).first()
+        return render_template("post.html", title=title, content=post.content)
 
 @post_pages.route("/post/", methods=["GET", "POST"])
 def create_post():
     if request.method == "POST":
-        pass
-    return "Create post page."
+        title = request.form.get("title")
+        content = request.form.get("content")
+        with Session(current_app.engine) as session:
+            session.add(Post(title=title, content=content))
+            session.commit()
+        return redirect(url_for(".display_post", title=title))
+    return render_template("new_post.html")
+
